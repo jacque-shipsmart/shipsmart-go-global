fea4847566299a2e9d05d046f91a889efeed6844
jacque-shipsmart <jacqueline.costa@shipsmart.global>
2025-06-23 18:34:44 +0100

    Add files via upload

---
+++ game_logic.py
@@ -0,0 +1,17 @@
+
+import random
+import json
+
+def jogar_dado():
+    return random.randint(1, 6)
+
+def obter_proxima_casa(atual, dado, max_casas):
+    nova = atual + dado
+    return nova if nova < max_casas else max_casas - 1
+
+def carregar_dados():
+    with open("data/casas.json", "r") as f:
+        casas = json.load(f)
+    with open("data/cartas.json", "r") as f:
+        cartas = json.load(f)
+    return casas, cartas
